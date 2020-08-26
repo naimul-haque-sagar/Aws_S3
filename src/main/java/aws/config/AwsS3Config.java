8214fd6b81eb331908ed089c13fb15ec22da0c6d
naimul-haque-sagar <naimulhaquesagar@gmail.com>
2020-08-27 00:21:43 +0600

    ignored some file

--- src/main/java/aws/config/AwsS3Config.java
+++ src/main/java/aws/config/AwsS3Config.java
@@ -1,6 +1,9 @@
 package aws.config;
 
 import com.amazonaws.services.s3.AmazonS3Client;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.amazonaws.auth.AWSCredentials;
@@ -10,18 +13,19 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 @Configuration
+@ConfigurationProperties("aws")
+@Data
 public class AwsS3Config {
+    private String accessKey;
+    private String secretKey;
+
     @Bean
     public AmazonS3 s3() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "Replace with your own",
-                "Replace with your own"
-        );
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey,secretKey);
         AmazonS3 amazonS3 = AmazonS3Client.builder()
                 .withRegion("ap-south-1")
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
-
         return amazonS3;
     }
 }
